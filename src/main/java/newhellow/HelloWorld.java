e45a1fa26bb8a804df8650db310e786ee627b222
NightWatchCoder <bradles789@gmail.com>
2025-06-27 13:26:45 +1000

    Updating the HelloWorld Java application to accept a parameter and output that parameter in the Hello World message.

--- src/main/java/newhellow/HelloWorld.java
+++ src/main/java/newhellow/HelloWorld.java
@@ -1,6 +1,11 @@
 package newhellow;
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        if(args.length > 0) {
+            System.out.println("Hello World: " + args[0] + "!");
+        } else {
+            System.out.println("Hello World!");
+        }
     }
 }
